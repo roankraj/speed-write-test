129ebfcc9e4690a92d01aededbb0793b29d337ec
roankraj <roankraj656@gmail.com>
2026-03-26 09:35:23 +0530

    optimization

--- js/result/script-result.js
+++ js/result/script-result.js
@@ -1,73 +1,55 @@
-const progressBar = document.getElementById("progress");
-const progressTrack = document.getElementById("progress-track");
-
-const wpmBox = document.getElementById("wpm");
-const wrongBox = document.getElementById("wrong-keys");
-const correctBox = document.getElementById("correct-keys");
-const totalBox = document.getElementById("total-keys");
-const percent = document.getElementById("percent");
-const difficulty = document.getElementById("difficulty");
-const lang = document.getElementById("lang");
-const timeSpan = document.getElementById("time");
-
-const playAgain = document.getElementById("play-again");
-const download = document.getElementById("download");
-
-const totalTime = Number(localStorage.getItem("total-time"));
-const remainingTime = Number(localStorage.getItem("time"));
-const time = totalTime - remainingTime;
-
-const wrong = Number(localStorage.getItem("wrong"));
-const totalKeys = Number(localStorage.getItem("totalKeys"));
-const redCount = Number(localStorage.getItem("redCount"));
-const index = Number(localStorage.getItem("index"));
-
-const wpm = time > 0 ? Math.round(totalKeys / (5 * (time / 60))) : 0;
-const correct = totalKeys - wrong;
-const accuracy = Math.round(100 * (1 - redCount / (index - 1)));
-
-wrongBox.innerText = wrong;
-correctBox.innerText = correct;
-totalBox.innerText = totalKeys;
-
-let wpmNow = 0;
-let progressNow = 0;
-let statsFinished = false;
-
-wpmBox.innerText = 0;
-percent.innerText = "0%";
-progressBar.style.width = "0%";
-
+const progressBar = document.getElementById("progress"),
+  progressTrack = document.getElementById("progress-track"),
+  wpmBox = document.getElementById("wpm"),
+  wrongBox = document.getElementById("wrong-keys"),
+  correctBox = document.getElementById("correct-keys"),
+  totalBox = document.getElementById("total-keys"),
+  percent = document.getElementById("percent"),
+  difficulty = document.getElementById("difficulty"),
+  lang = document.getElementById("lang"),
+  timeSpan = document.getElementById("time"),
+  playAgain = document.getElementById("play-again"),
+  download = document.getElementById("download"),
+  totalTime = Number(localStorage.getItem("total-time")),
+  remainingTime = Number(localStorage.getItem("time")),
+  time = totalTime - remainingTime,
+  wrong = Number(localStorage.getItem("wrong")),
+  totalKeys = Number(localStorage.getItem("totalKeys")),
+  redCount = Number(localStorage.getItem("redCount")),
+  index = Number(localStorage.getItem("index")),
+  wpm = time > 0 ? Math.round(totalKeys / ((time / 60) * 5)) : 0,
+  correct = totalKeys - wrong,
+  accuracy = Math.round(100 * (1 - redCount / (index - 1)));
+((wrongBox.innerText = wrong),
+  (correctBox.innerText = correct),
+  (totalBox.innerText = totalKeys));
+let diffStr,
+  langStr,
+  wpmNow = 0,
+  progressNow = 0,
+  statsFinished = !1;
 function animateStats() {
-  let running = false;
-
-  if (wpmNow < wpm) {
-    wpmNow++;
-    wpmBox.innerText = wpmNow;
-    running = true;
-  }
-
-  if (progressNow < accuracy) {
-    progressNow++;
-    progressBar.style.width = progressNow + "%";
-    percent.innerText = progressNow + "%";
-    running = true;
-  }
-
-  if (running) {
-    requestAnimationFrame(animateStats);
-  } else {
-    wpmBox.innerText = wpm;
-    progressBar.style.width = accuracy + "%";
-    percent.innerText = accuracy + "%";
-    statsFinished = true;
-  }
+  let e = !1;
+  (wpmNow < wpm && (wpmNow++, (wpmBox.innerText = wpmNow), (e = !0)),
+    progressNow < accuracy &&
+      (progressNow++,
+      (progressBar.style.width = progressNow + "%"),
+      (percent.innerText = progressNow + "%"),
+      (e = !0)),
+    e
+      ? requestAnimationFrame(animateStats)
+      : ((wpmBox.innerText = wpm),
+        (progressBar.style.width = accuracy + "%"),
+        (percent.innerText = accuracy + "%"),
+        (statsFinished = !0)));
 }
-
-animateStats();
-
-let diffStr;
-switch (Number(localStorage.getItem("difficulty"))) {
+switch (
+  ((wpmBox.innerText = 0),
+  (percent.innerText = "0%"),
+  (progressBar.style.width = "0%"),
+  animateStats(),
+  Number(localStorage.getItem("difficulty")))
+) {
   case 0:
     diffStr = "easy";
     break;
@@ -76,44 +58,31 @@ switch (Number(localStorage.getItem("difficulty"))) {
     break;
   case 2:
     diffStr = "hard";
-    break;
 }
-
-let langStr;
 switch (Number(localStorage.getItem("lang"))) {
   case 0:
     langStr = "english";
     break;
   case 1:
     langStr = "hindi";
-    break;
 }
-
-timeSpan.innerText = `${totalTime}s`;
-difficulty.innerText = diffStr;
-lang.innerText = langStr;
-
-playAgain.addEventListener("click", function () {
-  window.location.href = "index.html";
-});
-
-download.addEventListener("click", async function () {
-  wpmBox.innerText = wpm;
-  progressBar.style.width = accuracy + "%";
-  percent.innerText = accuracy + "%";
-
-  await new Promise((r) => setTimeout(r, 80));
-
-  const card = document.querySelector("main");
-
-  const canvas = await html2canvas(card, {
-    useCORS: true,
-    backgroundColor: "#182926",
-    scale: window.devicePixelRatio,
-  });
-
-  const link = document.createElement("a");
-  link.download = "result-card.png";
-  link.href = canvas.toDataURL();
-  link.click();
-});
+((timeSpan.innerText = `${totalTime}s`),
+  (difficulty.innerText = diffStr),
+  (lang.innerText = langStr),
+  playAgain.addEventListener("click", function () {
+    window.location.href = "index.html";
+  }),
+  download.addEventListener("click", async function () {
+    ((wpmBox.innerText = wpm),
+      (progressBar.style.width = accuracy + "%"),
+      (percent.innerText = accuracy + "%"),
+      await new Promise((e) => setTimeout(e, 80)));
+    const e = document.querySelector("main"),
+      t = await html2canvas(e, {
+        useCORS: !0,
+        backgroundColor: "#182926",
+        scale: window.devicePixelRatio,
+      }),
+      n = document.createElement("a");
+    ((n.download = "result-card.png"), (n.href = t.toDataURL()), n.click());
+  }));
